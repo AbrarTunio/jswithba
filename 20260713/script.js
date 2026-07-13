82510e677dd545de65b1a5ed70a7c7ad3daadc2c
AbrarTunio <abrar.tunio@yahoo.com>
2026-07-13 19:24:30 +0500

    Code Integrated

---
+++ 20260713/script.js
@@ -0,0 +1,8 @@
+let username = "Baakh";
+let brothername = "Aalik";
+
+document.getElementsByTagName("h1")[0].innerText = username;
+document.getElementsByTagName("h1")[1].innerText = brothername;
+
+// console.log("My Name is", username);
+// console.log("My Brother's Name is", brothername);
