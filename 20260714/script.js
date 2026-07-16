20ad3ee9cb898334814e55120bc4911cd8089564
AbrarTunio <abrar.tunio@yahoo.com>
2026-07-16 19:35:02 +0500

    js selector

---
+++ 20260714/script.js
@@ -0,0 +1,5 @@
+let num1 = 7;
+let num2 = 4;
+result = (4 + 5) * 2 + 5;
+
+console.log(`${result}`);
---
+++ 20260716/script.js
@@ -0,0 +1,11 @@
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+
+a = 8;
+b = 6;
+
+sum = a + b;
+mul = a * b;
+
+h1.innerText = sum;
+h2.textContent = mul;
